aa522c4d46f2f2b4e8c7cebdd593e869a6b15e4f
Lucyz23 <lucymagalhaesbarbosa@gmail.com>
2022-11-29 22:53:01 -0300

    feat(header): adicionado search abrir e fechar e testes

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -19,6 +19,18 @@ describe('Testes do componente <Header />', () => {
     expect(screen.getByTestId(SEARCH_B)).toBeInTheDocument();
   });
 
+  it('Testa se o search abre e fecha ao ser clicado', () => {
+    renderWithRouterAndRedux(<Header title="Meals" search />);
+
+    userEvent.click(screen.getByTestId(SEARCH_B));
+
+    expect(screen.getByTestId('search-input')).toBeInTheDocument();
+
+    userEvent.click(screen.getByTestId(SEARCH_B));
+
+    expect(screen.queryByTestId('search-input')).not.toBeInTheDocument();
+  });
+
   it('Testa se o search não aparece em certas paginas', () => {
     renderWithRouterAndRedux(<Header title="Profile" />);
 
