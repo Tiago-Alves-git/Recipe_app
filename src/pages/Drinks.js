8fa3539723c8d7839626a8cbc9ca5281c8b4cd39
Pablo Silva Sousa <pablopablito156@gmail.com>
2022-11-30 19:21:53 -0300

    feat: footer check

--- src/pages/Drinks.js
+++ src/pages/Drinks.js
@@ -1,8 +1,12 @@
+import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 function Drinks() {
   return (
-    <Header search title="Drinks" />
+    <div>
+      <Header search title="Drinks" />
+      <Footer />
+    </div>
   );
 }
 
