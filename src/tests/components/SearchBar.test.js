6a05d7ff7ef2c5565a40423a7148f686401ce164
Pablo Silva Sousa <pablopablito156@gmail.com>
2022-12-02 00:44:14 -0300

    feat: tests searchBar

---
+++ src/tests/components/SearchBar.test.js
@@ -0,0 +1,94 @@
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { renderWithRouterAndRedux } from '../../helpers/renderWith';
+import SearchBar from '../../components/SearchBar';
+
+describe('Testes do componente <SearchBar />', () => {
+  const ingredientId = 'ingredient-search-radio';
+  const inputId = 'search-input';
+  const buttonId = 'exec-search-btn';
+  it('', async () => {
+    renderWithRouterAndRedux(<SearchBar />, { initialEntries: ['/meals'] });
+    const searchInput = await screen.findByTestId(inputId);
+    userEvent.type(searchInput, 'beef');
+    const inputRadious = await screen.findByTestId(ingredientId);
+    userEvent.click(inputRadious);
+    const button = await screen.findByTestId(buttonId);
+    userEvent.click(button);
+    const receita = await screen.findByTestId('0-recipe-card');
+    expect(receita).toBeInTheDocument();
+  });
+
+  it('', async () => {
+    renderWithRouterAndRedux(<SearchBar />, { initialEntries: ['/meals'] });
+    const searchInput = await screen.findByTestId(inputId);
+    userEvent.type(searchInput, 'beef');
+    const inputRadious = await screen.findByTestId('first-letter-search-radio');
+    userEvent.click(inputRadious);
+    const button = await screen.findByTestId(buttonId);
+    userEvent.click(button);
+  });
+
+  it('', async () => {
+    renderWithRouterAndRedux(<SearchBar />, { initialEntries: ['/meals'] });
+    const button = await screen.findByTestId(buttonId);
+    userEvent.click(button);
+  });
+
+  it('', async () => {
+    renderWithRouterAndRedux(<SearchBar />, { initialEntries: ['/drinks'] });
+    const button = await screen.findByTestId(buttonId);
+    userEvent.click(button);
+  });
+
+  it('', async () => {
+    renderWithRouterAndRedux(<SearchBar />, { initialEntries: ['/meals'] });
+    const searchInput = await screen.findByTestId(inputId);
+    userEvent.type(searchInput, 'Banana Pancakes');
+    const inputRadious = await screen.findByTestId('name-search-radio');
+    userEvent.click(inputRadious);
+    const button = await screen.findByTestId(buttonId);
+    userEvent.click(button);
+  });
+});
+
+describe('', () => {
+  const ingredientId = 'ingredient-search-radio';
+  const inputId = 'search-input';
+  const buttonId = 'exec-search-btn';
+  it('', async () => {
+    renderWithRouterAndRedux(<SearchBar />, { initialEntries: ['/drinks'] });
+    const searchInput = await screen.findByTestId(inputId);
+    userEvent.type(searchInput, 'pineapple');
+    const inputRadious = await screen.findByTestId(ingredientId);
+    userEvent.click(inputRadious);
+    const button = await screen.findByTestId(buttonId);
+    userEvent.click(button);
+    const receita = await screen.findByTestId('0-recipe-card');
+    expect(receita).toBeInTheDocument();
+    userEvent.click(receita);
+  });
+
+  it('', async () => {
+    renderWithRouterAndRedux(<SearchBar />, { initialEntries: ['/drinks'] });
+    const searchInput = await screen.findByTestId(inputId);
+    userEvent.type(searchInput, 'apple');
+    const inputRadious = await screen.findByTestId(ingredientId);
+    userEvent.click(inputRadious);
+    const button = await screen.findByTestId(buttonId);
+    userEvent.click(button);
+  });
+});
+
+// describe('', () => {
+//   it('', async () => {
+//     renderWithRouterAndRedux(<SearchBar />, { initialEntries: ['/meals'] });
+//     const searchInput = await screen.findByTestId('search-input');
+//     userEvent.type(searchInput, 'potato');
+//     const inputRadious = await screen.findByTestId('ingredient-search-radio');
+//     userEvent.click(inputRadious);
+//     const button = await screen.findByTestId('exec-search-btn');
+//     userEvent.click(button);
+//   });
+// });
