ee99b52d4d78117d2b9878ada041071a16c32b84
Pablo Silva Sousa <pablopablito156@gmail.com>
2022-12-05 21:36:03 -0300

    feat: profile, faltando testes

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,10 +1,53 @@
+import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 function Profile() {
+  const { email } = JSON.parse(localStorage.getItem('user'));
+  const history = useHistory();
+
+  const doneRecipes = () => {
+    history.push('/done-recipes');
+  };
+
+  const favoriteRecipes = () => {
+    history.push('/favorite-recipes');
+  };
+
+  const logout = () => {
+    localStorage.clear();
+    history.push('/');
+  };
+
   return (
     <div>
       <Header title="Profile" />
+      <p data-testid="profile-email">{email}</p>
+      <button
+        type="button"
+        data-testid="profile-done-btn"
+        onClick={ doneRecipes }
+      >
+        Done Recipes
+
+      </button>
+      <button
+        type="button"
+        data-testid="profile-favorite-btn"
+        onClick={ favoriteRecipes }
+      >
+        Favorite Recipes
+
+      </button>
+      <button
+        type="button"
+        data-testid="profile-logout-btn"
+        onClick={ logout }
+      >
+        Logout
+
+      </button>
       <Footer />
     </div>
   );
