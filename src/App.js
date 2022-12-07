10a8df812fb09818b4be1c6efa395fa75a2e7e18
Guilherme Alves <guilherme@bigmidia.com>
2022-12-07 18:40:18 -0300

    fix: Rota para receitas in progress

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Login from './pages/Login';
 import Profile from './pages/Profile';
 import RecipeDetails from './pages/RecipeDetails';
 import Recipes from './pages/Recipes';
+import teste from './components/teste';
 
 import './App.css';
 
@@ -15,13 +16,13 @@ function App() {
       <Route exact path="/" component={ Login } />
       <Route exact path="/profile" component={ Profile } />
 
+      <Route path="/drinks/:id/:in-progress" component={ teste } />
       <Route exact path="/drinks" component={ Recipes } />
       <Route path="/drinks/:id" component={ RecipeDetails } />
-      <Route path="/drinks/:id/:in-progress?" component={ RecipeDetails } />
 
+      <Route path="/meals/:id/:in-progress" component={ teste } />
       <Route exact path="/meals" component={ Recipes } />
       <Route path="/meals/:id" component={ RecipeDetails } />
-      <Route path="/drinks/:id/:in-progress?" component={ RecipeDetails } />
 
       <Route exact path="/done-recipes" component={ DoneRecipes } />
       <Route exact path="/favorite-recipes" component={ FavoriteRecipes } />
